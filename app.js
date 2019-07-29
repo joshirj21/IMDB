7673501b171b5c0c14ef396141e2ad03818d8ce2
Rahul joshi <joshirj@android-2120ee3b45.localdomain>
2019-07-29 17:03:34 +0530

    29-7-19

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-    res.send("This is the landing page");
+    res.redirect("/imdb");
+})
+
+app.get("/imdb", function (req, res) {
+    res.send("This is the landing page")
+})
+
+app.get("/show", function (req, res) {
+    res.send("This is the show route")
 })
 
 app.listen(3000, function (err) {
