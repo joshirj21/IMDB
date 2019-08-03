9104a86d38d0bf466362172e51049773d2c44df5
Rahul joshi <joshirj@android-2120ee3b45.localdomain>
2019-08-03 12:36:16 +0530

    Added bootstrap and navbar

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express();
 var request = require("request")
 
 app.set("view engine", "ejs");
+app.use(express.static('public'))
+
 app.get("/", function (req, res) {
-    res.redirect("/imdb");
+    res.render("index1");
 })
 
 app.get("/imdb", function (req, res) {
@@ -13,22 +15,18 @@ app.get("/imdb", function (req, res) {
         request("http://www.omdbapi.com/?s=" + search + "&apikey=5e74b332", function (err, response, body) {
             if (!err && response.statusCode === 200) {
                 var parsedData = JSON.parse(body);
-                // console.log(parsedData.Search[0])
                 res.render("index", { imdb: parsedData })
             }
         })
     }
 })
 
-app.get("/search", function (req, res) {
-    res.render("index1")
-})
 
 app.get("/show", function (req, res) {
     res.send("This is the show route")
 })
 
-app.listen(3000, function (err) {
+app.listen(5000, function (err) {
     if (err) {
         console.log(err)
     }
