3ff85bf78fa6aee73f76739f61348afb935630e7
Rahul joshi <joshirj@android-2120ee3b45.localdomain>
2019-08-10 18:43:07 +0530

    done with image transform

--- v5/public/script/app.js
+++ v5/public/script/app.js
@@ -7,4 +7,37 @@
 //     this.classList.toggle("show");
 //     document.querySelector(".dropdown-menu").classList.toggle("show")
 // })
+// document.getElementsByClassName(".founded")
+// setInterval(function () {
 
+// }, 4000);
+// var zoomImage = document.getElementsByClassName("zoomin");
+// var i = 0;
+// while (i < zoomImage.length) {
+//     zoomImage[i].addEventListener("mouseenter", function () {
+//         this.classList.add("zoomIn", "animated");
+//     })
+//     zoomImage[i].addEventListener("mouseleave", function () {
+//         this.classList.remove("zoomIn", "animated");
+//     })
+//     i++;
+// }
+// var j = 0;
+// while (j < zoomImage.length) {
+//     zoomImage[j].addEventListener("mouseenter", function () {
+//         console.log("zooom it")
+//         this.classList.add("zoomIn", "animated");
+//     })
+//     i++;
+// }
+var i = 0;
+var zoomimage = document.getElementsByClassName("zoomin");
+while (i < zoomimage.length) {
+    zoomimage[i].addEventListener("mouseenter", function () {
+        this.classList.add("info");
+    })
+    zoomimage[i].addEventListener("mouseleave", function () {
+        this.classList.remove("info");
+    })
+    i++;
+}
